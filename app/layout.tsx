842609864ec5109dfba5a7e21f720d1a013ca62a
milagros <mdoldan@tecnoazar.com.ar>
2024-12-20 12:16:47 -0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "./ui/global.css";
+import { montserrat } from "./ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        </body> 
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lusitana, Montserrat } from "next/font/google";
+
+export const montserrat = Montserrat({ subsets: ["latin"], weight: "400"})
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400"})
